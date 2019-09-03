72542475182f82875af5bdd224c7f9a29ee27530
fmahiant <fmahiant@users.noreply.github.com>
2019-09-03 21:20:26 +0200

    Use implementation of glassfish-corba instead of only API

--- src/main/java/org/openmodelica/IModelicaRecord.java
+++ src/main/java/org/openmodelica/IModelicaRecord.java
@@ -7,7 +7,7 @@ import java.util.Map;
  * Instead, they are tagged using this interface... Thus, external
  * Java uniontypes also use this function for consistency.
  */
-public interface IModelicaRecord extends ModelicaObject,Map<String,ModelicaObject> {
+public interface IModelicaRecord extends ModelicaObject, Map<String,ModelicaObject> {
   /** ctor_index is -2 for ModelicaRecord and should be set to
    * -1 for any regular record that is returned to OMC.
    * The DefinitionsCreator will create record classes that fulfill
